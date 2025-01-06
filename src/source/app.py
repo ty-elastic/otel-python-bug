3c0212d9cbd0e09be3e683f04046bc1864378e9e
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2025-01-06 14:23:24 -0600

    add manual span example

--- src/source/app.py
+++ src/source/app.py
@@ -4,6 +4,8 @@ import requests
 import time
 from threading import Thread
 
+from opentelemetry import trace
+
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 
@@ -15,6 +17,8 @@ def health():
 def request():
     return f"OK"
 
+tracer = trace.get_tracer("source")
+
 def generate_request(*, request_id):
     try:
         response = requests.post(f"http://127.0.0.1:5000/request")
@@ -22,11 +26,17 @@ def generate_request(*, request_id):
     except Exception as inst:
         print(inst)
 
+@tracer.start_as_current_span("manual_span")
+def manual_span():
+    return "OK"
+
 def generate_requests():
     request_id = 0
     while True:
         request_id = request_id+1
         generate_request(request_id=request_id)
+        time.sleep(1)
+        manual_span()
         time.sleep(1)
 
 Thread(target=generate_requests, daemon=False).start()
